faeeb6d88b64b5edee0a1661bf4fc9a284bfa492
giaongo <83873333+giaongo@users.noreply.github.com>
2025-10-27 10:48:22 +0200

    Add postgres database

--- superset_config.py
+++ superset_config.py
@@ -7,7 +7,16 @@ FEATURE_FLAGS = {
 
 # Flask configuration
 ENABLE_PROXY_FIX = True
-SECRET_KEY = "YOUR_OWN_RANDOM_GENERATED_STRING"
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# Database configuration
+DATABASE_DB=os.environ.get("DATABASE_DB")
+DATABASE_HOST=os.environ.get("DATABASE_HOST")
+DATABASE_PASSWORD=os.environ.get("DATABASE_PASSWORD")
+DATABASE_USER=os.environ.get("DATABASE_USER")
+DATABASE_PORT=os.environ.get("DATABASE_PORT")
+DATABASE_DIALECT=postgresql
 
 # Ensure logs are initialized
 STORE_LOGS_REMOTELY = True

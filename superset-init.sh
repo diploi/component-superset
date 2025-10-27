697fc27ce8e2b955b47b12f421b6be16fd15ef8d
giaongo <83873333+giaongo@users.noreply.github.com>
2025-10-27 13:19:49 +0200

    Debug DB connection

--- superset-init.sh
+++ superset-init.sh
@@ -10,9 +10,13 @@ superset db upgrade
 echo "Initializing roles and permissions..."
 superset init
 
+echo "Configuring database connection..."
+superset set_database_uri --database_name my_postgres --uri "$SQLALCHEMY_DATABASE_URI"
+
 # Create Admin user, you can read these values from env or anywhere else possible
 echo "Creating admin user..."
 superset fab create-admin --username "$ADMIN_USERNAME" --firstname Superset --lastname Admin --email "$ADMIN_EMAIL" --password "$ADMIN_PASSWORD" || true
+
 
 # Starting server
 echo "Starting Superset server..."
--- superset_config.py
+++ superset_config.py
@@ -18,5 +18,16 @@ DATABASE_USER=os.environ.get("DATABASE_USER")
 DATABASE_PORT=os.environ.get("DATABASE_PORT")
 DATABASE_DIALECT="postgresql"
 
+print("Configuring database connection...")
+print(f"DB Host: {DATABASE_HOST}")
+print(f"DB Port: {DATABASE_PORT}")
+print(f"DB User: {DATABASE_USER}")
+print(f"DB Name: {DATABASE_DB}")
+SQLALCHEMY_DATABASE_URI = (
+    f"{DATABASE_DIALECT}://{DATABASE_USER}:{DATABASE_PASSWORD}@"
+    f"{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DB}"
+)
+
+print(f"SQLALCHEMY_DATABASE_URI: {SQLALCHEMY_DATABASE_URI}")
 # Ensure logs are initialized
 STORE_LOGS_REMOTELY = True
